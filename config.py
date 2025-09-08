b4109a5662ec3cff755e947fc7fdf2163e45443e
Ali1a8 <almajd99ali@gmail.com>
2025-09-08 21:32:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,9 @@ PAYMENT_ALERTS_CHANNEL = -1002719649863
 SUBSCRIBE_CHANNEL_ID = -1002804590079
 SUPPORT_USERNAME = "AboAlmajd18"
 
-# PostgreSQL Connection Settings
-DB_HOST = "dpg-d23vame3jp1c73ae9650-a"
+DB_HOST = "dpg-d2vhevje5dus73fn4bdg-a.oregon-postgres.render.com"
 DB_PORT = 5432
-DB_NAME = "dbmaster_yehk"
-DB_USER = "dbmaster_yehk_user"
-DB_PASSWORD = "FL6nmuzBXyf2EpnPQmYrHMTI0C2tc6Q0"
-DATABASE_URL = "postgresql://dbmaster_yehk_user:FL6nmuzBXyf2EpnPQmYrHMTI0C2tc6Q0@dpg-d23vame3jp1c73ae9650-a.oregon-postgres.render.com:5432/dbmaster_yehk"
+DB_NAME = "master_g4au"
+DB_USER = "master_g4au_user"
+DB_PASSWORD = "dVcOu6R7GkXxACT3Z6k9taWaFzUIboRt"
+DATABASE_URL = "postgresql://master_g4au_user:dVcOu6R7GkXxACT3Z6k9taWaFzUIboRt@dpg-d2vhevje5dus73fn4bdg-a.oregon-postgres.render.com/master_g4au"
